71550e21fcdeadba52466e0b9e75f084ca107ee3
xie <1442199759@qq.com>
2023-11-17 18:42:12 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import {ref,onMounted} from 'vue'
+import {gsap} from 'gsap'
+import HelloWorld from './components/HelloWorld.vue'
+import MyLink from './components/MyLink.vue'
+const de = ref(null)
+console.log(de.value)
+onMounted(()=>{
+  console.log(de.value)
+  gsap.to(de.value,{
+    rotation:27,
+    direction:2
+  })
+})
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank"  >
+      <img src="/vite.svg" class="logo" alt="Vite logo" ref="de" v-for="ot in 20" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo"/>
+    </a>
+  </div>
+  <MyLink v-for="item in 90"/>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/MyLink.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Link</div>
+</template>
